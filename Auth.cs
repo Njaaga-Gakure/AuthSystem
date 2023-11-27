cc1290984311e9441418690898f56cc23728ec33
Njaaga-Gakure <njaagagakure@gmail.com>
2023-11-27 17:43:04 +0300

    Implement login method

--- Auth.cs
+++ Auth.cs
@@ -11,11 +11,12 @@ namespace AuthSystem
         public void register() {
             while (true)
             {
+                Console.WriteLine("\n\n\n\t\t\t  Register Form");
                 Console.WriteLine("Enter username");
                 string? userName = Console.ReadLine();
                 Console.WriteLine("Enter password");
                 string? password = Console.ReadLine();
-                string path = @"C:\Users\User\desktop\a.txt";
+                string path = @"C:\Users\a.txt";
                 if (userName != null && password != null)
                 {
                     if (!File.Exists(path)) {
@@ -35,7 +36,52 @@ namespace AuthSystem
         }
 
         public void login() {
-            Console.WriteLine("login");
+            string path = @"C:\Users\a.txt";
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("User does not exist... :(");
+                return;
+            }
+
+            var users = File.ReadAllLines(path);
+
+            Dictionary<string, string> usersData = new Dictionary<string, string>();
+            
+            foreach ( var user in users)
+            {
+                string[] credentials =  user.Split(" ");
+                string userName = credentials[0];
+                string password = credentials[1];
+                usersData.Add(userName, password);
+            }
+
+            do
+            {
+                Console.WriteLine("\n\n\n\t\t\tLogin Form");
+                Console.WriteLine("Enter username: ");
+                string? loginUserName = Console.ReadLine();
+                Console.WriteLine("Enter password: ");
+                string? loginPassword = Console.ReadLine();
+
+                if (loginUserName != null && loginPassword != null) {
+                    if (!usersData.ContainsKey(loginUserName))
+                    {
+                        Console.WriteLine("User does not exist. Plese try again... :(");
+                        continue;
+                    }
+                    else if (usersData[loginUserName] != loginPassword)
+                    {
+                        Console.WriteLine("Invalid password. Please try again... :(");
+                        continue;
+                    }
+                    else {
+                        Console.WriteLine($"Welcome, {loginUserName}");
+                        break;
+                    }
+                }
+
+            } while (true);
         }
 
     }
