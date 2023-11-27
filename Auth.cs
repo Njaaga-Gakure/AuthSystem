0a3c1081441dc767dd179069d91d2fea2bf26478
Njaaga-Gakure <njaagagakure@gmail.com>
2023-11-27 16:32:05 +0300

    Initial commit

---
+++ Auth.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthSystem
+{
+    class Auth
+    {
+        public void register() {
+            while (true)
+            {
+                Console.WriteLine("Enter username");
+                string? userName = Console.ReadLine();
+                Console.WriteLine("Enter password");
+                string? password = Console.ReadLine();
+                string path = @"C:\Users\User\desktop\a.txt";
+                if (userName != null && password != null)
+                {
+                    if (!File.Exists(path)) {
+                        File.Create(path);
+                        File.WriteAllText(path, $"{userName} {password}");
+                        break;
+                    }
+                    File.AppendAllText(path, $"\n{userName} {password}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please input all the fields... :(");
+                }
+
+            }
+        }
+
+        public void login() {
+            Console.WriteLine("login");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using AuthSystem;
+
+Auth auth = new Auth();
+
+
+
+do
+{
+    Console.WriteLine("Choose either register of login: ");
+    Console.WriteLine("Choose option 1 to login or option 2 to register");
+    var option = Console.ReadLine();
+    if (option != null)
+    {
+        bool isNumber = int.TryParse(option, out var number);
+        if (isNumber)
+        {
+            if (number == 1)
+            {
+                auth.login();
+                break;
+            }
+            else if (number == 2)
+            {
+                auth.register();
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option. Please try again.");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid option. Please try again.");
+        }
+    }
+} while (true);
